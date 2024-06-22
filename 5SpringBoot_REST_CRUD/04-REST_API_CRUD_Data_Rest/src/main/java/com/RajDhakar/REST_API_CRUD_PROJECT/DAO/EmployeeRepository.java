72b10b8d82d0dfb2e81a5e852392f9dabc07f7f9
Raj-dhakar <rajdhakar062003@gmail.com>
2024-06-22 15:11:23 +0530

    ALmost Done

--- 5SpringBoot_REST_CRUD/04-REST_API_CRUD_Data_Rest/src/main/java/com/RajDhakar/REST_API_CRUD_PROJECT/DAO/EmployeeRepository.java
+++ 5SpringBoot_REST_CRUD/04-REST_API_CRUD_Data_Rest/src/main/java/com/RajDhakar/REST_API_CRUD_PROJECT/DAO/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.RajDhakar.REST_API_CRUD_PROJECT.DAO;
 
 import com.RajDhakar.REST_API_CRUD_PROJECT.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
 
     // that's it no need to write any code
